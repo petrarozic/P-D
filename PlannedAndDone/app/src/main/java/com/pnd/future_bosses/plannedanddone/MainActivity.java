a088a25487f47e1dbd7c09f5ea3fbe0f95cea376
Petra Rozic <perozic@pr1-03.studenti.math.hr>
2019-02-07 16:54:17 +0100

    Dodana ikona plus, prilagoden optionsMenu

--- PlannedAndDone/app/src/main/java/com/pnd/future_bosses/plannedanddone/MainActivity.java
+++ PlannedAndDone/app/src/main/java/com/pnd/future_bosses/plannedanddone/MainActivity.java
@@ -67,10 +67,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_category) {
             return true;
         }
-
+*/
         return super.onOptionsItemSelected(item);
     }
 
