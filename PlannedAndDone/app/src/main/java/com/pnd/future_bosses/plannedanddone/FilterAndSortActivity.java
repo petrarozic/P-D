edd1b6549334d2d8c9287a82bea14d8bd2fe6d1c
Sara Puzar <sapuzar@pr3-09.studenti.math.hr>
2019-02-13 18:43:49 +0100

    Punjenje kategorija za srot i filter.

--- PlannedAndDone/app/src/main/java/com/pnd/future_bosses/plannedanddone/FilterAndSortActivity.java
+++ PlannedAndDone/app/src/main/java/com/pnd/future_bosses/plannedanddone/FilterAndSortActivity.java
@@ -1,12 +1,22 @@
 package com.pnd.future_bosses.plannedanddone;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class FilterAndSortActivity extends AppCompatActivity {
 
@@ -14,6 +24,37 @@ public class FilterAndSortActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_and_sort);
+
+        LinearLayout layoutCategories = (LinearLayout) findViewById(R.id.Categories);
+        layoutCategories.removeAllViews();
+        Uri table = Uri.parse( "content://hr.math.provider.contprov/category");
+
+        Cursor c;
+        if (android.os.Build.VERSION.SDK_INT <11) {
+            //---before Honeycomb---
+            c = managedQuery(table, null, null, null, null);
+        } else {
+            //---Honeycomb and later---
+            CursorLoader cursorLoader = new CursorLoader(
+                    this,
+                    table, null, null, null, null);
+            c = cursorLoader.loadInBackground();
+        }
+        if (c.moveToFirst())
+        {
+            do {
+                CheckBox newCategory = new CheckBox(this);
+                newCategory.setText(c.getString(1));
+                newCategory.setTag(c.getInt(0));
+                layoutCategories.addView(newCategory);
+
+            } while (c.moveToNext());
+        }
+        else{
+            TextView newCategory = new TextView(this);
+            newCategory.setText("You don't have any categories yet.");
+            layoutCategories.addView(newCategory);
+        }
     }
 
 
